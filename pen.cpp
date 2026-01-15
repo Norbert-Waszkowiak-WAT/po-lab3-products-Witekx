a018798a4364b403fade14485664d499740237be
Alfonsy Ogórek <witold.wesolowski@woli.edu.pl>
2026-01-15 08:00:52 +0000

    zadanie dla chetnych

---
+++ pen.cpp
@@ -0,0 +1,14 @@
+#ifndef PEN
+#define PEN
+ 
+#include "paper_product.cpp"
+using namespace std;
+ 
+class Pen : public PaperProduct {
+    public:
+    string getName() override {
+        return "Pen";
+    }
+};
+ 
+#endif
